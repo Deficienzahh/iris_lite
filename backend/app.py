72a41db8ccfb04e713c6d7d0a8b0e37a7d604e2e
Deficienzahh <121560901+Deficienzahh@users.noreply.github.com>
2025-09-01 11:06:56 +0200

    railway json

--- backend/app.py
+++ backend/app.py
@@ -68,5 +68,5 @@ def serve_frontend(path):
 
 # --- Main ---
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5100))
     app.run(host="0.0.0.0", port=port)
